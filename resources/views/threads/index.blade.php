ff5b9a2689dd805dc2882a5b402ff13a5d719f10
Anjo Tadena <anjo@coderepubliq.com>
2018-01-01 08:49:41 +0800

    Add thread resources.

---
+++ resources/views/threads/index.blade.php
@@ -0,0 +1,20 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+            <div class="card">
+                <div class="card-header">Forum Threads</div>
+
+                <div class="card-body">
+                   	@foreach ($threads as $thread)
+						<h4><a href="{{ route('threads.show', $thread->id) }}">{{ $thread->title }}</a></h4>
+						<div class="body">{{ $thread->body }}</div>
+                   	@endforeach
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
