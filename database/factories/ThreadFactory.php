35a2c4a7ee17457e1e81f81379077cefc1e4efb3
Anjo Tadena <anjo@coderepubliq.com>
2018-05-27 17:22:33 +0800

    Add Thread Factory.

---
+++ database/factories/ThreadFactory.php
@@ -0,0 +1,17 @@
+<?php
+
+use Faker\Generator as Faker;
+
+// $table->string('title');
+// $table->text('body');
+
+// $table->unsignedInteger('user_id');
+$factory->define(App\Thread::class, function (Faker $faker) {
+    return [
+        'title' => $faker->sentence,
+        'body' => $faker->paragraph(3),
+        'user_id' => function () {
+        	return factory(\App\User::class)->create()->id;
+        }
+    ];
+});
