2d6560158e5e6e26b3ed2ad9164ad4481bd6bb30
Anjo Tadena <anjo@coderepubliq.com>
2018-05-28 10:44:27 +0800

    Refactor thread reply.

--- resources/views/threads/show.blade.php
+++ resources/views/threads/show.blade.php
@@ -5,7 +5,10 @@
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
-                <div class="card-header">{{ $thread->title }}</div>
+                <div class="card-header">
+                    <a href="#">{{ $thread->creatorName }}</a>
+                    Posted: {{ $thread->title }}
+                </div>
 
                 <div class="card-body">
                     {{ $thread->body }}
@@ -21,14 +24,7 @@
             <h3>Replies</h3>
 
             @foreach ($thread->replies as $reply)
-            <div class="panel">
-                <div class="panel-header"><a href="#">{{ $reply->owner->name }}</a> said {{ $reply->created_at->diffForHumans() }}...</div>
-
-                <div class="panel-body">
-                    {{ $reply->body }}
-                </div>
-            </div>
-            <hr>
+                @include ('threads.reply')
             @endforeach
         </div>
     </div>
