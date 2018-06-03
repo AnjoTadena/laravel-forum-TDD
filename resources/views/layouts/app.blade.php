238baf1cca1c1461ed6ad23561ba7eed591687f0
Anjo Tadena <anjo@coderepubliq.com>
2018-06-03 10:21:30 +0800

    Add channels dropdown to the header nav.

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -35,6 +35,17 @@
                     <!-- Left Side Of Navbar -->
                     <ul class="navbar-nav mr-auto">
                         <li><a class="nav-link" href="{{ route('threads.index') }}">All Threads</a></li>
+                        <li class="nav-item dropdown">
+                            <a id="channel-dropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+                                Channels <span class="caret"></span>
+                            </a>
+
+                            <div class="dropdown-menu" aria-labelledby="channel-dropdown">
+                                @foreach (\App\Channel::all() as $channel)
+                                <a class="dropdown-item" href="/threads/{{$channel->slug}}">{{ $channel->name }}</a>
+                                @endforeach
+                            </div>
+                        </li>
                     </ul>
 
                     <!-- Right Side Of Navbar -->
