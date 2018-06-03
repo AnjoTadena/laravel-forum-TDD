6a49d6bfe757ccad17825cce7b4470a41356aa78
Anjo Tadena <anjo@coderepubliq.com>
2018-06-03 10:48:28 +0800

    Populate validation errors, error data, and list of all channels.

--- resources/views/threads/create.blade.php
+++ resources/views/threads/create.blade.php
@@ -9,14 +9,30 @@
         </div>
         
         <div class="col-md-12">
+            @if ($errors->count())
+            <ul class="alert alert-danger">
+                @foreach ($errors->all() as $error)
+                    <li>{{ $error }}</li>
+                @endforeach
+            </ul>
+            @endif
             <form action="{{ route('threads.channel.store')}}" method="POST">
                 {{ csrf_field() }}
                 <div class="form-group">
-                    <input class="form-control" id="title" name="title" placeholder="TItle" required/>
+                    <select class="form-control" id="channel_id" name="channel_id">
+                        <option value="">Please Select a channel</option>
+                        @foreach (\App\Channel::all() as $channel)
+                        <option value="{{$channel->id}}" {{ $channel->id == old('channel_id') ? 'selected' : '' }}>{{$channel->name}}</option>
+                        @endforeach
+                    </select>
                 </div>
 
                 <div class="form-group">
-                    <textarea class="form-control" name="body" id="body" rows="5" placeholder="body" required></textarea>
+                    <input class="form-control" id="title" name="title" placeholder="TItle" value="{{old('title')}}"/>
+                </div>
+
+                <div class="form-group">
+                    <textarea class="form-control" name="body" id="body" rows="5" placeholder="body" required>{{old('body')}}</textarea>
                 </div>
 
                 <div class="form-group">
