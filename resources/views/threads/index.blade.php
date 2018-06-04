97fc7181387e784bd4361770c4f9a06c2980860b
Anjo Tadena <anjo@coderepubliq.com>
2018-06-04 20:30:02 +0800

    Display number of replies in every threads.

--- resources/views/threads/index.blade.php
+++ resources/views/threads/index.blade.php
@@ -11,7 +11,10 @@
         @foreach ($threads as $thread)
             <div class="col-md-8" style="margin: 10px 0;">
                 <div class="card">
-                    <div class="card-header"><a href="{{ route('threads.channel.show', ['channel' => $thread->channel->slug, 'thread' => $thread->id]) }}">{{ $thread->title }}</a></div>
+                    <div class="level card-header">
+                        <a class="flex" href="{{ route('threads.channel.show', ['channel' => $thread->channel->slug, 'thread' => $thread->id]) }}">{{ $thread->title }}</a>
+                        <strong><a href="{{$thread->path()}}">{{$thread->replies_count . ' ' . str_plural('reply', $thread->replies_count)}}</a></strong>
+                    </div>
                     <div class="card-body">
 						<div class="body">{{ $thread->body }}</div>
                     </div>
