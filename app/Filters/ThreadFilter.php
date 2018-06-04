dfad7df8b3e57c7de01c836bc4bc070d20ebaf93
Anjo Tadena <anjo@coderepubliq.com>
2018-06-04 20:28:12 +0800

    Add filter for popular threads.

--- app/Filters/ThreadFilter.php
+++ app/Filters/ThreadFilter.php
@@ -7,12 +7,19 @@ use App\Contracts\FilterContract;
 
 class ThreadFilter extends FilterContract
 {
-	protected $filters = ['by'];
+	protected $filters = ['by', 'popular'];
 
 	protected function by($name)
 	{
 		$user = User::where('name', $name)->firstOrFail();
 
         return $this->builder->where('user_id', $user->id);
+	}
+
+	protected function popular()
+	{
+		$this->builder->getQuery()->orders = [];
+
+		return $this->builder->orderBy('replies_count', 'desc');
 	}
 }
