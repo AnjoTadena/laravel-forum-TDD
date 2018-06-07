642b92e8efb1ae29ab38c8ef09494d4e2e63e09d
Anjo Tadena <anjo@coderepubliq.com>
2018-06-07 16:48:36 +0800

    Cache channels.

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use App\Channel;
+use Illuminate\Support\Facades\Cache;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -16,7 +17,12 @@ class AppServiceProvider extends ServiceProvider
     {
         // \View::share('channels', Channel::all());
         \View::composer('*', function ($view) {
-            $view->with('channels', Channel::all());
+            
+            $channels = Cache::rememberForever('channels', function () {
+                return Channel::all();
+            });
+
+            $view->with('channels', $channels);
         });
     }
 
